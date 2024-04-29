6c57365d784c0b34ef7b975fed6061f7e84b6584
Milutin25 <stefanovicfamily@Stefanovics-iMac.local>
2024-04-29 23:42:09 +0200

    Quiz app

---
+++ main.js
@@ -0,0 +1,95 @@
+/*
+STEP 1. create array of objects with questions, asnwers and declare index for correct answer
+STEP 2. declare variable for current question index so quiz would start from the first question = 0 
+and declare variable for score that is going to be displayed = 0
+STEP 3. make function that will load questions
+STEP 4. creating a function that will be called when answer button is clicked
+STEP 5. creating funstion that will display final score at the end
+*/
+
+
+
+//STEP 1//
+const questions = [
+    {
+        question: "Who was MVP of 2022/2023 NBA season",
+        answers: ["Lebron James", "Nikola Jokic", "Kevin Durant", "Luka Doncic"],
+        correct: 1
+    },
+    {
+        question: "Who is the tennis GOAT?",
+        answers: ["Marat Safin", "Ivan Lendl", "Boris Becker", "Novak Djokovic"],
+        correct: 3
+    },
+    {
+        question: "Select European Champions League winner for 1991",
+        answers: ["Bayern Munich", "AC MIlan", "Red Star Belgrade", "Partizani Tirana"],
+        correct: 2
+    },
+    {
+        question: "100 meters World record belongs to?",
+        answers: ["Usain Bolt", "Carl Lewis", "Slobodan Brankovic", "Wilson Boit Kipketer"],
+        correct: 0
+    }
+];
+
+//STEP 2//
+let currentQuestionIndex = 0;
+let score = 0;
+
+//STEP 3//
+function loadQuestions(){
+    const questionElement = document.getElementById("question");
+    const answerElement = document.getElementById("answers");
+
+    answerElement.innerHTML = "";
+
+    const currentQuestion = questions[currentQuestionIndex];//currentQuestion is the first object in array
+    questionElement.textContent = currentQuestion.question;
+
+    currentQuestion.answers.forEach((answer,index) => {
+        const button = document.createElement("button");//creating button for each answer
+        button.className = "answer";// creating class for this button
+        button.textContent = answer;//creating text for button which is text in object under "answers"
+        button.addEventListener("click", () => selectAnswer(index));// on button click we call function that takes index of selected answer
+        answerElement.appendChild(button);
+    });
+};
+
+//STEP 4
+
+function selectAnswer(index){
+    const currentQuestion = questions[currentQuestionIndex];
+
+    if (index === currentQuestion.correct){ //if index is same to correct answer index from object
+        score++;
+    }
+
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){//moving to next question
+        loadQuestions();
+    }else{
+    displayScore()//call function that will display score at the end, and will define this function below
+    }
+}
+function displayScore(){
+    const scoreElement = document.getElementById("score");
+    scoreElement.textContent = `Your score is ${score} out of ${questions.length}.`;
+}
+
+    document.getElementById("nextQuestion").addEventListener("click", () =>{
+        if (currentQuestionIndex < questions.length){
+            loadQuestions();
+        }
+    });
+
+document.addEventListener("DOMContentLoaded", loadQuestions);
+
+
+
+
+
+
+
+
+
